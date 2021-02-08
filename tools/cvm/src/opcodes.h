0a952c2640e16a5cd37d32b54fe78c52cb0deb37
Jirapas Jipipob <78689828+ackerman2001@users.noreply.github.com>
2021-02-09 04:47:14 +0700

    Update opcodes.h

--- tools/cvm/src/opcodes.h
+++ tools/cvm/src/opcodes.h
@@ -1,6 +1,20 @@
 #include<stdio.h>
 
-void execution(unsigned short opcode)
+void execution(unsigned short opcode, unsigned char *memory, int *bc, long *R)
 {
-	printf("0x%.4x \n", opcode);
+	//printf("pc: %x : 0x%.4x \n", *bc/2, opcode);
+	//for debugging
+	switch(opcode&0xFF)
+	{
+		case 0x0D:
+			R[0xA] =  opcode>>8;
+			break;
+		case 0xFF:
+			for(long int i=0; i<R[opcode>>12]; i++)
+			{
+				printf("%c", memory[1048576+*bc+2+i]);
+			}
+			printf("\n");
+			break;
+	}
 }
