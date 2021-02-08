f758a63197ceb5d416eba4db824855d4f62a7ae8
ackerman2001 <78689828+ackerman2001@users.noreply.github.com>
2021-02-08 20:54:35 +0700

    Update cvm.c

--- tools/cvm/src/cvm.c
+++ tools/cvm/src/cvm.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-int main()
+int main(int argc, char** argv)
 {
-	printf("Hello, Caitlyn"); //print a text message.
+	FILE *file;
+	char *filename = argv[1]; //assign *filename to argument 1
+	file = fopen(filename, "rb"); //load file
+	if (file == NULL)
+	{
+		printf("Could not open file.\n");
+		exit(-1);
+	}
 	return 0; //return value from a function.
 }
