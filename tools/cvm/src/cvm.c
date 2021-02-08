70e1773c4b290e08fc5c2af857ee4e2770f2d197
ackerman2001 <78689828+ackerman2001@users.noreply.github.com>
2021-02-08 21:33:43 +0700

    Update cvm.c

--- tools/cvm/src/cvm.c
+++ tools/cvm/src/cvm.c
@@ -3,13 +3,18 @@
 
 int main(int argc, char** argv)
 {
+	short opcode;
+	int file_size;
 	FILE *file;
 	char *filename = argv[1]; //assign *filename to argument 1
 	file = fopen(filename, "rb"); //load file
-	if (file == NULL)
+	if (file == NULL) //if file doesn't exist
 	{
 		printf("Could not open file.\n");
 		exit(-1);
 	}
+	fseek(file, 0, SEEK_END); //point to the end of the file
+	file_size = ftell(file);
+	printf("%d\n", file_size);
 	return 0; //return value from a function.
 }
