4854183c041f81758e2068d33f6b8d4db0210f32
Jirapas Jipipob <78689828+ackerman2001@users.noreply.github.com>
2021-02-09 21:28:55 +0700

    Update opcodes.h

--- tools/cvm/src/opcodes.h
+++ tools/cvm/src/opcodes.h
@@ -2,8 +2,23 @@
 
 void execution(unsigned short opcode, unsigned char *memory, int *pc, long *R)
 {
-	switch(opcode&0xFF)
+	unsinged char b[7];
+	for(int i = 0; i < 7; i++)
 	{
-			
+		b[i] = memory[pc + 1 + i];	
+	}
+	switch(opcode)
+	{
+		case 0x00:
+			R[A] = (b[7] << 56) | (b[6] << 48) | (b[5] << 40) | (b[4] << 32) | (b[3] << 24) | (b[2] << 16) | (b[1] << 8) | b[0];
+			pc += 8;
+			break;
+		case 0xFF:
+			while(memory[R[A]] != 0x00)
+			for(int i = 0; memory[R[A]+i] != 0x00; i++)
+			{
+				printf("%c", memory[R[A] + i]);	
+			}
+			break;
 	}
 }
