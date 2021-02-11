2b2c6963cc15134da2605577a01d6bd6c410412d
Jirapas Jipipob <78689828+ackerman2001@users.noreply.github.com>
2021-02-11 22:34:41 +0700

    Update opcodes.h

--- tools/cvm/src/opcodes.h
+++ tools/cvm/src/opcodes.h
@@ -13,6 +13,14 @@ void execution(unsigned char opcode, unsigned char *memory, int *pc, long *R)
 			R[0xA] = (b[7] << 56) | (b[6] << 48) | (b[5] << 40) | (b[4] << 32) | (b[3] << 24) | (b[2] << 16) | (b[1] << 8) | b[0];
 			*(pc) += 8;
 			break;
+		case 0x01:
+			for(int i = 0; i < 8; i++) 
+			{
+				b[i] = memory[*pc + 1 + i];
+			}
+			R[0xB] = (b[7] << 56) | (b[6] << 48) | (b[5] << 40) | (b[4] << 32) | (b[3] << 24) | (b[2] << 16) | (b[1] << 8) | b[0];
+			*(pc) += 8;
+			break;
 		case 0xFE:
 			for(int i = 0; i < 2048; i++)
 			{
