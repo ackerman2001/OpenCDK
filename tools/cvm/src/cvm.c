a75808e8f8b8af083a18482af73ec5b04e5f129f
Nutchanon Charnwutiwong <79015379+nutchanon-c@users.noreply.github.com>
2021-02-14 00:02:56 +0700

    Update cvm.c

--- tools/cvm/src/cvm.c
+++ tools/cvm/src/cvm.c
@@ -39,7 +39,8 @@ int main(int argc, char** argv)
 	for(int pc = 0; pc < file_size; pc = pc + 1)
 	{
 		execution(g_memory[pc], g_memory, &pc, R, window, &CW);
-		if(CW == 1) {
+		if(CW == 1) 
+		{
 			window = glfwCreateWindow(640,480, "Hello World", NULL, NULL);
 			CW == 0;
 		}
