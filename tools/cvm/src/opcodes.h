2da6e400d42fcbcecbda460237f9a30e5bb969f4
Jirapas Jipipob <78689828+ackerman2001@users.noreply.github.com>
2021-02-16 01:35:45 +0700

    Update opcodes.h

--- tools/cvm/src/opcodes.h
+++ tools/cvm/src/opcodes.h
@@ -114,7 +114,7 @@ void execution(unsigned char opcode, unsigned char *memory, int *pc, long *R, GL
 			}
 			break;
 		case 0xFB:
-			printf("%ld", memory[R[0xA]]);
+			printf("%ld", (b[7] << 56) | (b[6] << 48) | (b[5] << 40) | (b[4] << 32) | (b[3] << 24) | (b[2] << 16) | (b[1] << 8) | b[0]);
 			break;
 		case 0xFC:
 			*CW = 1;
